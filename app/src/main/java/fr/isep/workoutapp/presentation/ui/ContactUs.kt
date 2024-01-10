28da28af1e7325a1193bb22cb17e07f1cdb2f5d0
victorhabila <48987601+victorhabila@users.noreply.github.com>
2024-01-10 23:30:39 +0100

    Contact us Page

--- app/src/main/java/fr/isep/workoutapp/presentation/ui/ContactUs.kt
+++ app/src/main/java/fr/isep/workoutapp/presentation/ui/ContactUs.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.carolmusyoka.workoutapp.R
 
 class ContactUs : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_us)
